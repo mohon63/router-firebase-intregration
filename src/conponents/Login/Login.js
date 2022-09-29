78a54d7020f4cb249769ff9099edbe7f9af9d43d
mohon63 <mohonhossen63@gmail.com>
2022-09-29 10:15:41 +0600

    added onAuthState Change

--- src/conponents/Login/Login.js
+++ src/conponents/Login/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import useFirebase from './../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h3>Please login</h3>
             <div style={{ margin: '20px' }}>
-                <button>Sign in google</button>
+                <button onClick={signInWithGoogle}>Sign in google</button>
             </div>
             <form action="">
                 <input type="text" placeholder='Your email' />
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,18 +1,39 @@
-import { useEffect } from "react";
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import app from './../firebase.init';
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged, signOut } from "firebase/auth";
+
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const useFirebase = () => {
     const [user, setUser] = useState({});
 
-    useEffect(() => {
-
-    }, []);
-
     const signInWithGoogle = () => {
-        console.log('signin in soon')
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const user = result.user;
+                setUser(user)
+                console.log(user)
+            })
     };
 
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => { })
+    }
 
-    return { user, signInWithGoogle }
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            setUser(user)
+        })
+    }, [])
+
+    return {
+        user,
+        handleSignOut,
+        signInWithGoogle
+    }
 };
 
+export default useFirebase;
