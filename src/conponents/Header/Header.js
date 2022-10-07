27bcd9a500ab1f69b8b05afc01327cbb2f0ee73f
mohon63 <mohonhossen63@gmail.com>
2022-10-07 22:53:21 +0600

    kjhkj

--- src/conponents/Header/Header.js
+++ src/conponents/Header/Header.js
@@ -9,6 +9,7 @@ const Header = () => {
         <div className='header'>
             <nav>
                 <Link to="/">Home</Link>
+                {/* <Link to="/">Home</Link> */}
                 <Link to="/products">Products</Link>
                 <Link to="/orders">Orders</Link>
                 <Link to="/register">Register</Link>
