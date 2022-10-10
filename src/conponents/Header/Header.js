f13b5809e1bc41f922a0d737fe64292990406250
mohon63 <mohonhossen63@gmail.com>
2022-10-10 21:57:48 +0600

    hh

--- src/conponents/Header/Header.js
+++ src/conponents/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import useFirebase from './../../hooks/useFirebase';
+import { getAuth, signOut } from 'firebase/auth';
+import app from './../../firebase.init';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const auth = getAuth(app);
 
 const Header = () => {
-    const { user, handleSignOut } = useFirebase();
+    const [user] = useAuthState(auth)
     return (
         <div className='header'>
             <nav>
@@ -15,7 +19,7 @@ const Header = () => {
                 <span>{user?.displayName && user.displayName}</span>
                 {user?.uid
                     ?
-                    <button onClick={handleSignOut}>Sign Out</button>
+                    <button onClick={() => signOut(auth)}>Sign Out</button>
                     :
                     <Link to="/login">Login</Link>
                 }
