e1234fbfa3b2a99fc72454cdb57239103d7f7042
mohon63 <mohonhossen63@gmail.com>
2022-10-11 14:36:50 +0600

    auth intregration

--- src/conponents/Login/Login.js
+++ src/conponents/Login/Login.js
@@ -2,16 +2,28 @@ import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { getAuth } from 'firebase/auth';
 import app from './../../firebase.init';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const auth = getAuth(app);
 
 const Login = () => {
     const [SignInWithGoogle, user] = useSignInWithGoogle(auth)
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location?.state?.from?.pathname || '/';
+
+    const handleGoogleSignIn = () => {
+        SignInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true })
+            })
+    }
     return (
         <div>
             <h3>Please login</h3>
             <div style={{ margin: '20px' }}>
-                <button onClick={() => SignInWithGoogle()}>Sign in google</button>
+                <button onClick={handleGoogleSignIn}>Sign in google</button>
             </div>
             <form action="">
                 <input type="text" placeholder='Your email' />
