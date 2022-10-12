1478be74ba16a328fdb75600e37e8e624644127f
mohon63 <mohonhossen63@gmail.com>
2022-10-12 22:37:31 +0600

    kjh

--- src/conponents/Header/Header.js
+++ src/conponents/Header/Header.js
@@ -15,7 +15,13 @@ const Header = () => {
                 <Link to="/">Home</Link>
                 <Link to="/products">Products</Link>
                 <Link to="/orders">Orders</Link>
-                <Link to="/register">Register</Link>
+                {
+                    user && <>
+                        <Link to="/vip">VIP</Link>
+                        {/* <Link to="/vip">VIP</Link> */}
+                    </>
+                }
+
                 <span>{user?.displayName && user.displayName}</span>
                 {user?.uid
                     ?
